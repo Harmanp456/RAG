8ab154f924652b42e05c8d2d2e1ead4098d90dd1
“Harmanpreet <iharmansaini51@gmail.com>
2026-09-05 09:57:32 +0530

    BIG pdf

--- document loaders/pdf.py
+++ document loaders/pdf.py
@@ -1,6 +1,13 @@
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain_text_splitters import TokenTextSplitter  
 data=PyPDFLoader("document loaders/GRU.pdf")
 docs=data.load()
+
+splitter=TokenTextSplitter(chunk_size=1000, chunk_overlap=10)  # Split the text into chunks of 1000 tokens with no overlap
+
+chunks=splitter.split_documents(docs)  # Split the documents into chunks
+
 #  har page ka ek doc create hoga if page 15 then there are 15 docs print (len(data))  # print the number of docs
-print (docs[14])
+print (len(chunks))
+
+print(chunks[0].page_content)  # print the number of chunks
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import os
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
-
 load_dotenv()
 
-data = PyPDFLoader("document loaders/GRU.pdf").load()
+data = PyPDFLoader("document loaders/deeplearning.pdf").load()
 
+splitter=   RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)  # Split the text into chunks of 1000 characters with 10 characters overlap
+chunk=splitter.split_documents(data)
 template = ChatPromptTemplate.from_messages(
     [
         ("system", "You are an AI that summarizes the text."),
@@ -18,7 +20,7 @@ template = ChatPromptTemplate.from_messages(
 
 model = ChatMistralAI(model="mistral-small-latest")
 
-prompt = template.format_messages(data=data[0].page_content)
+prompt = template.format_messages(data=chunk[0].page_content)
 result = model.invoke(prompt)
 
 print(result.content)
